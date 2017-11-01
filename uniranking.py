dec613aeb7d27ca8d8810544f6a9b25a8227d860
leoheyns <leo@iris-advies.com>
2017-11-01 16:23:14 +0100

    Add files via upload

--- uniranking.py
+++ uniranking.py
@@ -1,7 +1,35 @@
 from sort import merge_pairs
+from eca import *
+
+from eca.generators import start_offline_tweets
+import datetime
+import textwrap
+
+
+@event('init')
+def setup(ctx, e):
+    start_offline_tweets('batatweets.txt', time_factor=None, event_name='chirp')
+
+
+@event('chirp')
+def tweet(ctx, e):
+    # we receive a tweet
+    tweet = e.data
+
+    # parse date
+    time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+
+    # nicify text
+    text = textwrap.fill(tweet['text'],initial_indent='    ', subsequent_indent='    ')
+    adjustranking(text.lower())
+
+    # generate output
+    output = "[{}] {} (@{}):\n{}".format(time, tweet['user']['name'], tweet['user']['screen_name'], text)
+    #emit('tweet', output)
+
 
 ulist = ["kub", "eur", "ru", "rug", "tu delft", "tue", "ul", "um", "ut", "uu", "uva", "vu", "wu"]
-uwords = [["tilburg", "kub"], ["erasmus", "rotterdam", "eur"], [], [], [], [], [], [], [], [], [], [], [], []]
+uwords = [["tilburg", "kub"], ["erasmus", "rotterdam", "eur"], ["radboud", "nijmegen", "ru"], ["rijksuniversiteit", "groningen", "rug"], ["tudelft", "delft", "tu delft"], ["eindhoven", "tue"], ["leiden", "ul"], ["maastricht", "um"], ["utwente", "enschede", "ut"], ["utrecht", "uu"], ["universiteit van amsterdam", "amsterdam", "uva"], ["vrije universiteit", "amsterdam", "vu"], ["wageningen", "wu"]]
 
 ranking = [0 for x in range(len(ulist)-1)]
 print(ranking)
@@ -12,22 +40,34 @@ def newranking(uid):
     ranking[uid] += 1
 
 
-def uwordsearch(uid, text):
+def uwordsearch(uid, text): #returns 1 if a word from the u
     global uwords
     i = 0
     while i < len(uwords[uid]):
+        iout = True
         if text.find(uwords[uid][i]) >= 0:
-            return 1
+            if text.find(uwords[uid][i]) > 0:
+                if str.isalpha(text[text.find(uwords[uid][i])-1]):
+                    iout = False
+            if text.find(uwords[uid][i])+len(uwords[uid][i]) < len(text):
+                if str.isalpha(text[text.find(uwords[uid][i])+len(uwords[uid][i])]):
+                    iout = False
+            if iout:
+                return 1
         i += 1
     return 0
 
-
-def adjustranking(text):
+#(not str.isalpha(text[text.find(uwords[uid][i])-1]) or not str.isalpha(text[text.find(uwords[uid][i])+len(uwords[uid][i])]))
+def adjustranking(text): #adjusts current ranking
     global ranking
-    ranking = [ranking[i] + uwordsearch(i, text) for i in range(len(ulist)-1)]
-
+    rankingtemp = [ranking[i] + uwordsearch(i, text) for i in range(len(ulist)-1)]
+    if ranking != rankingtemp:
+        ranking = rankingtemp
+        print(orderedranking()) #emits new ordered ranking if rankings changed
+    ranking = rankingtemp
 
 def orderedranking():
-    rank = merge_pairs([(ranking[i], ulist[i]) for i in range(len(ulist)-1)])
+    rank = merge_pairs([(ranking[i], uwords[i][0]) for i in range(len(ulist)-1)])
     rank.reverse()
     return rank
+
